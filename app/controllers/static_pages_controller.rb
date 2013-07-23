55b19361206b3bd1222d3296dadd44ea31ee9f47
Bakytzhan Baizhikenov <bahakz@gmail.com>
2013-07-22 21:58:30 -0700

    second commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def courses
+  end
+
+  def about
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Intellection::Application.config.secret_key_base = '2a50e9ab05434dd107a7b785e8be40f13ba7e6e6b3872726700e3e0fba3bf0107d6fa09d8d178149adca12a2c5949dd84af50260824e4a153411a1e5938d7690'
