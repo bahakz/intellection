a99581630325805a0337b9eaaa584264ff1036c6
Bakytzhan Baizhikenov <bahakz@gmail.com>
2013-07-22 22:30:58 -0700

    static_pages polished and contacts paged added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Intellection::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/courses"
-  get "static_pages/about"
+  root  'static_pages#home'
+  match '/home',    to: 'static_pages#home',    via: 'get'
+  match '/courses',   to: 'static_pages#courses',   via: 'get'
+  match '/about', to: 'static_pages#about', via: 'get'
+  match '/contacts', to: 'static_pages#contacts', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
