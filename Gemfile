2bbc7ffc3ebf239d26f4cbdd8450f1788d1df8cf
Bakytzhan Baizhikenov <bahakz@gmail.com>
2013-07-25 01:41:39 -0700

    Finish actions related to basic users

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ gem 'rails', '4.0.0'
 
 gem 'bootstrap-sass'
 gem 'bcrypt-ruby', '~> 3.0.0'
+gem 'faker', '1.1.2'
+gem 'will_paginate', '3.0.4'
+gem 'bootstrap-will_paginate', '0.0.9'
 
 # Use postgresql as the database for Active Record
 gem 'pg'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
+
   def new
     @user = User.new
   end
@@ -7,7 +11,11 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
-def create
+  def index
+    @users = User.paginate(page: params[:page]); 
+  end
+
+  def create
     @user = User.new(user_params)
     if @user.save
       sign_in @user
@@ -18,11 +26,48 @@ def create
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Изменения сохранены"
+      sign_in @user
+      redirect_to @user    
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "Пользователь удален."
+    redirect_to users_url
+  end
+
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+    end
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Пожалуйста, войдите в систему." 
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
     end
 
 end
